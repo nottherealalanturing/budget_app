807b5c237f9ae71b37179048bbde79451d28ea9f
Assad Isah <nottherealalanturing@gmail.com>
2022-10-10 13:45:51 +0100

    install active_storage for handling image uploads

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
     belongs_to :author, class_name: "User" , foreign_key: "author_id", dependent: :destroy
     has_many :trade_groups
     has_many :trade, through: :trade_groups
-    
+    has_one_attached :icon
+
     validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :trades, foreign_key: "author_id", dependent: :destroy
     has_many :groups, dependent: :destroy
+    has_one_attached :avatar
     
     validates :name, presence: true
     validates_associated :trade, :group
