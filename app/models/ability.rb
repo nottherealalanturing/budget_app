4cb9aad2c417aff7f45e78d46d670e2b8e655179
Assad Isah <nottherealalanturing@gmail.com>
2022-10-18 16:55:03 +0100

    add authorisation

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :manage, Group, author: user
+    can :manage, Entity, author: user
+  end
+end
